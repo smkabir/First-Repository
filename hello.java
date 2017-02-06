6db28c452b35ec666cc430c2ef43c07f36efd8e1
kabirs041 <kabirs041@.strose.edu>
2017-02-06 11:06:37 -0500

    conflict resolved?

--- hello.java
+++ hello.java
@@ -1,5 +1,14 @@
+
 public class hello{
-	public static void main(String[] args){
+
+	Public static void main(String[] args){
 		System.out.println("My name is SM");
+		System.out.println("This is Caitlin, and this is a collaboration.");
 	}
+
+
 }
+
+
+
+
